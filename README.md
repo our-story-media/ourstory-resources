1f1020e36d4adc7231838e252f1f650df3e1567d
Tom Bartindale <tom.bartindale@monash.edu>
2019-06-07 10:48:28 +1000

    added rtl support for ar

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Resources
 
 Branding, documentation and printable based resources for using Our Story.
+
+## Documentation
+
+These docs are based on https://vuepress.vuejs.org.
+
+Setup Editing:
+
+`npm install -g vuepress`
+
+To preview changes on you machine:
+
+`npm run docs:dev`
+
+Edit files in the `doc_gen` folder.
+
+To deploy to live site:
+
+`npm run docs:build && git commit -am"docs update" && git push`
---
+++ doc_gen/.vuepress/enhanceApp.js
@@ -0,0 +1,23 @@
+export default ({
+    Vue, // the version of Vue being used in the VuePress app
+    options, // the options for the root Vue instance
+    router, // the router instance for the app
+    siteData // site metadata
+  }) => {
+    // ...apply enhancements to the app
+    router.beforeEach((to, from, next) => {
+        /* must call `next` */
+        // console.log(to);
+        if (to.path.startsWith('/ar/'))
+        {
+            // console.log('RTL')
+            document.documentElement.setAttribute("dir","rtl");
+            // console.log(document.);
+        }
+        else
+        {
+            document.documentElement.setAttribute("dir","");
+        }
+        next();
+      })
+  }
